e1d14fb89269d7e3e4f3067063e8bab1d2e2207c
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-11-20 21:04:38 -0500

    casting

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -9,7 +9,19 @@ public class Main {
 
        short myMinShortValue = Short.MIN_VALUE;
        short myMaxShortValue = Short.MAX_VALUE;
+
+       long myMinLongValue = Long.MIN_VALUE;
+       long myMaxLongValue = Long.MAX_VALUE;
         System.out.println("Short min is equal to " + myMinShortValue);
         System.out.println("Short max is equal to " + myMaxShortValue);
+
+        System.out.println("long min value is " + myMinLongValue);
+        System.out.println("long max value is " + myMaxLongValue);
+
+        long myLong = 100L;
+        long myMinLong =Long.MIN_VALUE;
+        long myMaxLong = Long.MAX_VALUE;
+        System.out.println("long min" + myMinLong);
+        System.out.println("long max " + myMaxLong);
     }
 }
