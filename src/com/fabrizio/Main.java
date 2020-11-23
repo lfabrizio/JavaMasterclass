9a461f786efa29e1270240266a2a70b9bee69af4
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-11-23 18:09:10 -0500

    float and double primitve types

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -1,5 +1,7 @@
 package com.fabrizio;
 
+import java.util.zip.DeflaterOutputStream;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,15 +9,23 @@ public class Main {
 
 
         System.out.println("Hello, Lou");
-        byte byteValue = 10;
-        short shortValue = 20;
-        int intValue = 50;
-        long longValue = 50000;
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloat = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float max value " + myMaxFloat);
 
-        long sum = (byteValue + shortValue + intValue);
-        System.out.println(longValue + 10L * sum);
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDouble = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double max value " + myMaxDouble);
+
+        int myIntValue = 5;
+        float myFloatValue = (float)5.25;
+        double myDoubleValue = 5.25d;
+
 
 
 
     }
 }
+
