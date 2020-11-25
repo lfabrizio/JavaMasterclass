d47b88af630aabee5ad4352e8fad7dec33fb3325
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-11-24 20:11:21 -0500

    string data type

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -5,22 +5,25 @@ import java.util.zip.DeflaterOutputStream;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	//8 primitive types byte, short, int , long, float, double, char, boolean
+        String myString= "This is a string";
+        System.out.println(myString);
+        myString = myString + ", and this is more";
+        myString = myString + " \u00A9 2020";
+        System.out.println("new string is now " + myString);
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("Last string is equal to " + lastString);
+        double doubleNumber = 120.47d;
+        lastString = lastString + doubleNumber;
+        System.out.println(lastString);
 
 
-        System.out.println("Hello, Lou");
-        char myChar = 'D';
-        char myUni = '\u0044';
-        System.out.println(myChar);
-        System.out.println(myUni);
 
-        char myCopyrightChar = '\u00A9';
-        System.out.println(myCopyrightChar);
-
-        boolean myTrueBooleanValue = true;
-        boolean myFalseBooleanValue = false;
-
-        boolean isCustomerOverTwentyOne = true;
 
 
 
