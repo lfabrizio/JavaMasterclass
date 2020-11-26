8cb87bfe48f62008a40acd2443cc984ae57764c7
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-11-25 21:44:12 -0500

    adjusted

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -5,22 +5,12 @@ import java.util.zip.DeflaterOutputStream;
 public class Main {
 
     public static void main(String[] args) {
-	//8 primitive types byte, short, int , long, float, double, char, boolean
-        String myString= "This is a string";
-        System.out.println(myString);
-        myString = myString + ", and this is more";
-        myString = myString + " \u00A9 2020";
-        System.out.println("new string is now " + myString);
-        String numberString = "250.55";
-        numberString = numberString + "49.95";
-        System.out.println(numberString);
-        String lastString = "10";
-        int myInt = 50;
-        lastString = lastString + myInt;
-        System.out.println("Last string is equal to " + lastString);
-        double doubleNumber = 120.47d;
-        lastString = lastString + doubleNumber;
-        System.out.println(lastString);
+        System.out.println("Hello, Lou ");
+	int result = 1 + 2; // 1 + 2 = 3;
+        System.out.println("1 + 2" + result);
+        int previousResult = result;
+        System.out.println("Return Previous result = " + previousResult);
+        result = result - 1;
 
 
 
