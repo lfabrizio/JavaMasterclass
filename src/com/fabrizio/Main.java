a2d13fe2ae7761c6b628f19bc3667a5ab04d78dc
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-11-26 21:02:47 -0500

    operators

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -11,6 +11,17 @@ public class Main {
         int previousResult = result;
         System.out.println("Return Previous result = " + previousResult);
         result = result - 1;
+        System.out.println("3 - 1" + result);
+        System.out.println("previous result" + previousResult);
+
+        result = result * 10;
+        System.out.println("2 * 10 = " + result);
+
+        result = result / 5;
+        System.out.println("20 / 5 " + result);
+
+        result = result % 3;
+        System.out.println("4 % 3 =" + result);
 
 
 
