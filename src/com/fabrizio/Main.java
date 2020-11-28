f88be95f324a673545bf246e00c6baf62f4b4b4a
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-11-28 17:00:43 -0500

    expressions

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -6,56 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, Lou ");
-	int result = 1 + 2; // 1 + 2 = 3;
-        System.out.println("1 + 2" + result);
-        int previousResult = result;
-        System.out.println("Return Previous result = " + previousResult);
-        result = result - 1;
-        System.out.println("3 - 1" + result);
-        System.out.println("previous result" + previousResult);
+        double kilos = (100 * 1.609344);
+        int highScore = 50;
+        if(highScore == 50){
+            System.out.println("high score ");
+        }
 
-        result = result * 10;
-        System.out.println("2 * 10 = " + result);
-
-        result = result / 5;
-        System.out.println("20 / 5 " + result);
-
-        result = result % 3;
-        System.out.println("4 % 3 =" + result);
-
-        // result = result + 1;
-        result++; // 1 +1 = 2
-        result--;
-        System.out.println("2 - 1 = " + result);
-
-        //result = result * 10
-        result *= 10;
-        System.out.println("3 * 10 = " + result);
-
-       int topScore = 100;
-       if(topScore >= 100){
-           System.out.println("you got the high score");
-       }
-       int secondTopScore = 60;
-       if((topScore > secondTopScore) && (topScore < 100)){
-           System.out.println("Greater than second top score and less than 100");
-       }
-
-       if((topScore > 90) || (secondTopScore <= 90)){
-           System.out.println("either or both conditions are true");
-       }
-
-       // operator challenge
-        int newValue = 50;
-       if(newValue == 50){
-           System.out.println("This ");
-       }
-
-       boolean isCar = false;
-       if(isCar ){
-           System.out.println("this is not supposed to happen");
-       }
-       boolean wasCar = isCar ? true : false;
+        int score = 100;
+        if(score > 99){
+            System.out.println("high score");
+            score = 0;
+        }
     }
-}
-
+    }
