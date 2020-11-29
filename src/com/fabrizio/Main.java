dbaa5e0c20bad2498f1fb50b484c3c489a389ded
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-11-29 11:17:40 -0500

    if else

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -5,16 +5,30 @@ import java.util.zip.DeflaterOutputStream;
 public class Main {
 
     public static void main(String[] args) {
-     int myVariable = 50;
-     if(myVariable ==50){
-         System.out.println("Printed");
-     }
-     myVariable++;
-     myVariable--;
-        System.out.println("This is a test");
+     boolean gameOver = true;
+     int score = 800;
+     int score2 = 10000;
+     int levelCompleted = 8;
+     int bonus = 200;
 
-        int anotherVariable = 50;
-        myVariable--;
-        System.out.println("This is another one");
-    }
+//     if(score < 5000 && score > 1000) {
+//         System.out.println("Your score was less 5000 but greater than 1000");
+//     } else if(score < 1000){
+//            System.out.println("Your score was less than 1000");
+//     }
+//     else{
+//            System.out.println("this happens");
+//        }
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("your final score " + finalScore);
+        }
+
+        if (gameOver) {
+            int finalScore = score2 + (levelCompleted * bonus);
+            System.out.println("Second score");
+        }
+
+     }
     }
