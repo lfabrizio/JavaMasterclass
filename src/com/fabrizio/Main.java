ea671ed3fa194288d6856ffbf1c54fed3ee46934
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-12-01 18:46:06 -0500

    methods

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -11,24 +11,19 @@ public class Main {
      int levelCompleted = 8;
      int bonus = 200;
 
-//     if(score < 5000 && score > 1000) {
-//         System.out.println("Your score was less 5000 but greater than 1000");
-//     } else if(score < 1000){
-//            System.out.println("Your score was less than 1000");
-//     }
-//     else{
-//            System.out.println("this happens");
-//        }
+     calculateScore(true, 800, levelCompleted, bonus);
+     calculateScore(true, 10000, 8, 200);
 
-        if (gameOver) {
+     }
+
+     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+
+        if(gameOver){
             int finalScore = score + (levelCompleted * bonus);
-            System.out.println("your final score " + finalScore);
+            finalScore += 2000;
+            System.out.println("Your final score was "  + finalScore);
+            return finalScore;
         }
-
-        if (gameOver) {
-            int finalScore = score2 + (levelCompleted * bonus);
-            System.out.println("Second score");
-        }
-
+        return -1;
      }
     }
