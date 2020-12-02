e8e3a35bc82d56db9afc7e77a76cc85d2ed3d935
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-12-01 19:50:53 -0500

    more methods and challenge

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -11,9 +11,46 @@ public class Main {
      int levelCompleted = 8;
      int bonus = 200;
 
-     calculateScore(true, 800, levelCompleted, bonus);
-     calculateScore(true, 10000, 8, 200);
+     int highScore = calculateScore(true, 800, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
+     score = 10000;
+     levelCompleted = 8;
+     bonus = 200;
+     highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
 
+
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Tim", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Bob", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Percy", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Gilbert", highScorePosition);
+     }
+
+
+
+
+     public static void displayHighScorePosition(String playerName, int highScorePosition){
+         System.out.println(playerName + " Managed to get into position " + highScorePosition + "on the high score table");
+     }
+
+     public static int calculateHighScorePosition(int playerScore){
+
+        if(playerScore >= 1000){
+            return 1;
+        } else if(playerScore >= 500 && playerScore < 1000){
+            return 2;
+        } else if(playerScore >= 100 && playerScore < 500){
+            return 3;
+        } else {
+            return 4;
+        }
      }
 
      public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
@@ -21,7 +58,6 @@ public class Main {
         if(gameOver){
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
-            System.out.println("Your final score was "  + finalScore);
             return finalScore;
         }
         return -1;
