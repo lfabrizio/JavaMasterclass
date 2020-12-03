3c8e65da6931a63bbba4549693b86bfe1773e5e1
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-12-03 17:35:21 -0500

    positive negative challenge

---
+++ src/CodingExercises/PositiveNegativeZero.java
@@ -0,0 +1,12 @@
+package CodingExercises;
+
+public class PositiveNegativeZero {
+    public static void checkNumber(int number){
+        if(number >  0){
+            System.out.println("Positive");
+        } else if(number < 0){
+            System.out.println("negative");
+        } else
+            System.out.println("zero");
+    }
+}
