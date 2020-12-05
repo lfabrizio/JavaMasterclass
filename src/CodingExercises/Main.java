295f7aa9779ff3ddc0f5264b21552ed692afc903
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-12-05 13:57:03 -0500

    speed converter

---
+++ src/CodingExercises/Main.java
@@ -0,0 +1,10 @@
+package CodingExercises;
+
+public class Main {
+    public static void main(String[] args) {
+       long miles =  SpeedConverter.toMilesPerHour(10.5);
+        System.out.println(miles);
+
+        SpeedConverter.printConversion(10.5);
+    }
+}
