1a31a5f6849a41fcbafaa717fdbc5e74da1432e1
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-12-06 10:36:32 -0500

    calculator

---
+++ src/CodingExercises/LeapYear.java
@@ -0,0 +1,11 @@
+package CodingExercises;
+
+public class LeapYear {
+    public static boolean isLeapYear(int year){
+        if(year >= 1){
+            return true;
+        } else{
+            return false;
+        }
+    }
+}
