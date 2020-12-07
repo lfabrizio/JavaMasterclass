219dc6f8c2bde125945492953ac714363216a261
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-12-07 17:21:34 -0500

    method overloading

--- src/CodingExercises/Main.java
+++ src/CodingExercises/Main.java
@@ -2,9 +2,20 @@ package CodingExercises;
 
 public class Main {
     public static void main(String[] args) {
-       long miles =  SpeedConverter.toMilesPerHour(10.5);
-        System.out.println(miles);
+   int newScore =  calculateScore("Lou", 500);
+        System.out.println("new score is " + newScore);
+        calculateScore(75);
+        
+    }
 
-        SpeedConverter.printConversion(10.5);
+    public static int calculateScore(String playername, int score){
+        System.out.println("Player " + playername + " scores" + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score){
+        System.out.println("someone" + score + "points");
+        return score * 1000;
+
     }
 }
