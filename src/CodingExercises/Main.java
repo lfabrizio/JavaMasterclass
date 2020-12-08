7c9be53513d590dcad0ce3da87709d7e64c03539
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-12-08 18:20:46 -0500

    seconds and mins challenge

--- src/CodingExercises/Main.java
+++ src/CodingExercises/Main.java
@@ -2,27 +2,6 @@ package CodingExercises;
 
 public class Main {
     public static void main(String[] args) {
-   int newScore =  calculateScore("Lou", 500);
-        System.out.println("new score is " + newScore);
-        calculateScore(75);
-
+        System.out.println("enter");
     }
-
-    public static int calculateScore(String playername, int score){
-        System.out.println("Player " + playername + " scores" + score + " points");
-        return score * 1000;
-    }
-
-    public static int calculateScore(int score){
-        System.out.println("someone" + score + "points");
-        return score * 1000;
-
-    }
-
-    public static int calculateScore(){
-        System.out.println("No player or score");
-        return 0;
-    }
-
-
 }
