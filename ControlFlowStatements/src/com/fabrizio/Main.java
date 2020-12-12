7b13012927ab2607a7612a94e9e9d747706bfb06
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-12-12 17:57:24 -0500

    edit

--- ControlFlowStatements/src/com/fabrizio/Main.java
+++ ControlFlowStatements/src/com/fabrizio/Main.java
@@ -3,64 +3,36 @@ package com.fabrizio;
 public class Main {
 
     public static void main(String[] args) {
-	// control flow
-        // the switch statement
-//        int value = 1;
-//        if(value == 1){
-//            System.out.println("value is one ");
-//        } else if(value == 2){
-//            System.out.println("value two");
-//        } else {
-//            System.out.println("was not 1 or 2");
-//        }
-
-//        int switchValue = 1;
-//         switch(switchValue){
-//             case 1:
-//                 System.out.println("Value one ");
-//                 break;
-//
-//             case 2:
-//                 System.out.println("value two");
-//                 break;
-//
-//             default:
-//                 System.out.println("neither");
-//                 break;
-//    }
-
-        char switchValue = 'A';
-        switch(switchValue){
-            case 'A':
-                System.out.println("It is A");
-                break;
-            case 'B':
-                System.out.println("this is B");
-                break;
-            case 'C':
-                System.out.println("this is c");
-                break;
-            case 'D':
-                System.out.println("this is d");
-                break;
-            case 'E':
-                System.out.println("this is e");
-                break;
-            default:
-                System.out.println("not found ");
-                break;
+        // control flow
+    }
+            public static void printDayOfTheWeek(int day){
+                switch(day){
+                    case 0:
+                        System.out.println("Sunday");
+                        break;
+                    case 1:
+                        System.out.println("Monday");
+                        break;
+                    case 2:
+                        System.out.println("Tuesday");
+                        break;
+                    case 3:
+                        System.out.println("Wednesday");
+                        break;
+                    case 4:
+                        System.out.println("Thursday");
+                        break;
+                    case 5:
+                        System.out.println("Friday");
+                        break;
+                    case 6:
+                        System.out.println("Saturday");
+                        break;
+                    default:
+                        System.out.println("Invalid day");
+                        break;
+                }
         }
-
-        String month = "January";
-        switch(month.toLowerCase()){
-            case "january":
-                System.out.println("Jan");
-            case "february":
-                System.out.println("feb");
-            default:
-                System.out.println("other");
-        }
-
 
     }
-}
+
--- ControlFlowStatements/src/com/fabrizio/Switch.java
+++ ControlFlowStatements/src/com/fabrizio/Switch.java
@@ -1,4 +1,38 @@
 package com.fabrizio;
 
 public class Switch {
-}
+
+    public static void main(String[] args) {
+
+    char switchValue = 'A';
+        switch(switchValue){
+        case 'A':
+            System.out.println("It is A");
+            break;
+        case 'B':
+            System.out.println("this is B");
+            break;
+        case 'C':
+            System.out.println("this is c");
+            break;
+        case 'D':
+            System.out.println("this is d");
+            break;
+        case 'E':
+            System.out.println("this is e");
+            break;
+        default:
+            System.out.println("not found ");
+            break;
+    }
+
+    String month = "January";
+        switch(month.toLowerCase()){
+        case "january":
+            System.out.println("Jan");
+        case "february":
+            System.out.println("feb");
+        default:
+            System.out.println("other");
+    }
+}}
