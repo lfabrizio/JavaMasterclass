dfa00dc835372aab3f2575d05d9da1917ce362d6
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-12-16 21:20:02 -0500

    while

--- ControlFlowStatements/src/com/fabrizio/Main.java
+++ ControlFlowStatements/src/com/fabrizio/Main.java
@@ -8,11 +8,14 @@ while(count != 5){
     System.out.println("Count value is " + count);
     count++;
 }
-
-for(count =1; count != 6; count++){
-    System.out.println("Count value is " + count);
+count = 1;
+while(true){
+    if(count == 5){
+        break;
+    }
+    System.out.println("count is " + count);
+    count++;
 }
-
     }
 }
 
