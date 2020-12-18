3fb61594c187db159a691c16ddb5d4a3b617a18c
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-12-18 17:08:48 -0500

    68

--- ControlFlowStatements/src/com/fabrizio/Main.java
+++ ControlFlowStatements/src/com/fabrizio/Main.java
@@ -3,19 +3,10 @@ package com.fabrizio;
 public class Main {
 
     public static void main(String[] args) {
-int count = 0;
-while(count != 5){
-    System.out.println("Count value is " + count);
-    count++;
-}
-count = 1;
-while(true){
-    if(count == 5){
-        break;
+
     }
-    System.out.println("count is " + count);
-    count++;
-}
+
+
     }
-}
+
 
