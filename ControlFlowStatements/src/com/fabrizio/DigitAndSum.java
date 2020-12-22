c9448e700785438483406191ed657f076891a849
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-12-22 16:42:19 -0500

    70

--- ControlFlowStatements/src/com/fabrizio/DigitAndSum.java
+++ ControlFlowStatements/src/com/fabrizio/DigitAndSum.java
@@ -1,4 +1,24 @@
 package com.fabrizio;
 
 public class DigitAndSum {
+    public static void main(String[] args) {
+        System.out.println("sum of digits in 125 is " + sumDigits(125));
+        System.out.println("sum of digits in -125 is " + sumDigits(-125));
+        System.out.println("sum of digits in 4 is " + sumDigits(4));
+    }
+
+    public static int sumDigits(int number){
+        if(number < 10){
+            return -1;
+        }
+        int sum = 0;
+        while(number > 0){
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+        return sum;
+    }
+
+
 }
