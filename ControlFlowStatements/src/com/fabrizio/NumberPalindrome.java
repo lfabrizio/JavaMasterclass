19a4eeb298f052f46e9c3f932d0aa36416e43810
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-12-27 16:50:32 -0500

    palindrome

--- ControlFlowStatements/src/com/fabrizio/NumberPalindrome.java
+++ ControlFlowStatements/src/com/fabrizio/NumberPalindrome.java
@@ -1,4 +1,9 @@
 package com.fabrizio;
 
 public class NumberPalindrome {
+    //find the reverse of the given number
+    public static boolean isPalindrome(int number){
+         return true;
+    }
+    return false;
 }
