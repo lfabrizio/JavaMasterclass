2244d2c40d691d1141cd78d785e037b19daccbca
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-12-29 17:09:10 -0500

    user input

--- ControlFlowStatements/src/com/fabrizio/Main.java
+++ ControlFlowStatements/src/com/fabrizio/Main.java
@@ -1,9 +1,17 @@
 package com.fabrizio;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+// reading user input
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter name : ");
+        String name = scanner.nextLine();
 
+        System.out.println(" your name is " + name);
+        scanner.close();
     }
 
 
