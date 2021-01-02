41e10bf80c002d374d4ac5f2923f9953b3187f0c
lfabrizio <louisfabrizio.student@careerdevs.com>
2021-01-02 14:48:50 -0500

    commit

---
+++ ControlFlowStatements/src/com/fabrizio/ReadingUserInputChallenge.java
@@ -0,0 +1,11 @@
+package com.fabrizio;
+
+import java.util.Scanner;
+
+public class ReadingUserInputChallenge {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    scanner.close();
+    }
+
+}
