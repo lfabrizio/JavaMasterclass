dcfb758cacb013e20e7a8858aa43aa9cbcfbd890
lfabrizio <louisfabrizio.student@careerdevs.com>
2021-01-03 16:57:30 -0500

    hasNextInt

--- ControlFlowStatements/src/com/fabrizio/ReadingUserInputChallenge.java
+++ ControlFlowStatements/src/com/fabrizio/ReadingUserInputChallenge.java
@@ -5,7 +5,18 @@ import java.util.Scanner;
 public class ReadingUserInputChallenge {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+
+    int counter = 0;
+    int sum = 0;
+
+    while(true){
+        int order = counter + 1;
+        System.out.println("enter " + order + "" );
+
+        boolean isAnInt = scanner.hasNextInt();
+    }
     scanner.close();
+
     }
 
 }
